ae308197fe6a644aa99cacb5b144c2c2a9a44c18
Pranoy Das <pranoy.mbstu12001@gmail.com>
2026-06-30 00:04:03 +0800

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            Set<Character> set = new HashSet<>();
+
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+
+                if (set.contains(board[i][j])) return false;
+                set.add(board[i][j]);
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            Set<Character> set = new HashSet<>();
+
+            for (int j = 0; j < 9; j++) {
+                if (board[j][i] == '.') continue;
+
+                if (set.contains(board[j][i])) return false;
+                set.add(board[j][i]);
+            }
+        }
+
+
+        for (int square = 0; square < 9; square++) {
+            Set<Character> set = new HashSet<>();
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    int row = (square / 3) * 3 + i;
+                    int col = (square % 3) * 3 + j;
+                    if (board[row][col] == '.') continue;
+
+                    if (set.contains(board[row][col])) return false;
+                    set.add(board[row][col]);
+                }
+            }
+        }
+
+        return true;
+    }
+}
