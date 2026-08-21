d2c0090125158bfbbc38231b2478abf20fe9165c
Pranoy Das <pranoy.mbstu12001@gmail.com>
2026-08-22 01:50:21 +0800

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isHappy(int n) {
+        if (n == 1) return true;
+
+        return isNumberHappy(n);
+    }
+
+    private boolean isNumberHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+
+        while (!set.contains(n)) {
+            set.add(n);
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n = n / 10;
+            }
+
+            if (sum == 1) return true;
+            n = sum;
+        }
+
+        return false;
+    }
+}
