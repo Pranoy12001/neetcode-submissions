54bad8933cb8176470bd9d99bddbce7b94637a92
Pranoy Das <pranoy.mbstu12001@gmail.com>
2026-08-29 02:40:28 +0800

    Add: max-consecutive-ones - submission-1

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int result = 0;
+        for (int num : nums) {
+            if (num != 1) {
+                result = Math.max(result, count);
+                count = 0;
+            } else {
+                count++;
+            }
+        }
+
+        return Math.max(result, count);
+    }
+}
