0db9f777416ec820b28f3067a5826a0e97ea2fca
Pranoy Das <pranoy.mbstu12001@gmail.com>
2026-09-18 01:27:12 +0800

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int i = 0;
+        int j = 1;
+        int maxProfit = 0;
+        while (j < prices.length) {
+            if (prices[i] > prices[j]) {
+                i = j;
+                j++;
+            } else {
+                maxProfit = Math.max(maxProfit, prices[j] - prices[i]);
+                j++;
+            }
+        }
+
+        return maxProfit;
+    }
+}
